0f389f474f2f480f2f937e3a4eb311da6e9ce662
Adithya Vijay K <adithyavj@gmail.com>
2021-02-26 22:30:54 +0530

    read write operations

---
+++ readwriteops.py
@@ -0,0 +1,15 @@
+a = 10
+b = 20
+c = a + b
+# To Print a Text in console
+print("Hello World", c)
+print("Sum of a: " + str(a) + " and b: " + str(b) + " is c: " + str(c))
+
+i = input("Enter a number")
+print('The value user entered is:', i)
+
+m = input("Enter two numbers")
+n = input()
+# values called by input numbers are string
+print("Numbers are: " + m + ", " + n)
+print("Sum of these numbers is "+str(int(m)+int(n)))
