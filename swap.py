48f81185def858525db6a60cb75c0e069947ae1c
Adithya Vijay K <adithyavj@gmail.com>
2021-02-26 22:41:36 +0530

    swap two numbers

---
+++ swap.py
@@ -0,0 +1,17 @@
+a = 10
+b = 20
+print("Value of A: " + str(a) + ",B: " + str(b))
+t = a
+a = b
+b = t
+print("Value after swapping A: " + str(a) + ",B: " + str(b))
+
+# Type of variable
+i = input("this is string")
+j = 90
+print(type(i))
+print(type(j))
+
+""" 
+    multiple lines of comment
+"""
