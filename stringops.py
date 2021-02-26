425170acf459bfbc11e199a819a4d65b015b4048
Adithya Vijay K <adithyavj@gmail.com>
2021-02-26 22:54:13 +0530

    list

--- stringops.py
+++ stringops.py
@@ -2,4 +2,12 @@
 a = "Hey, how are you"
 print(len(a))
 print(a[5])
-print(a[5:8]) # to print from one position to another of an array
+print(a[5:8])  # to print from one position to another of an array
+
+# string values are immutable
+
+# List in python
+names = ["apple", "orange", "mango"]
+names[0] = "changed"  # list is mutable, values can be changed
+print(names)
+print(len(names))
