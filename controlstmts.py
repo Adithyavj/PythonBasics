a95bd0fcf82a953baff48a50f14bf6c457958de5
Adithya Vijay K <adithyavj@gmail.com>
2021-02-26 23:18:37 +0530

    loop ops

--- controlstmts.py
+++ controlstmts.py
@@ -1,3 +1,4 @@
+# if else and elif
 num = input("Enter a number")
 
 if int(num) < 0:
@@ -6,3 +7,13 @@ elif int(num) == 0:
     print("Entered number is zero")
 else:
     print("Entered Number is positive")
+
+# Loops
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+else:
+    print("Loop Finished")
+
+
