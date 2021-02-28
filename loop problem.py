539cde925baa48899faa58a89eb805e3f32773a9
Adithya Vijay K <adithyavj@gmail.com>
2021-02-28 22:27:40 +0530

    loop to print pyramid

--- loop problem.py
+++ loop problem.py
@@ -4,18 +4,22 @@
        * *
       * * *
 """
-i = int(input("Enter number of rows:"))
-j = int(input("Enter number of columns:"))
-pchr = "*"
-count=0
-while count<i:
-    count1=0
-    
-    while count1<j:
-        print("*", end="")
-        count1=count1+1
-    else:
-        print("\r")
-    count=count+1
-else:
-    print("Finished")
+# i = int(input("Enter number of rows:"))
+# j = int(input("Enter number of columns:"))
+# pchr = "*"
+# count=0
+# while count<i:
+#     count1=0
+#
+#     while count1<j:
+#         print("*", end="")
+#         count1=count1+1
+#     else:
+#         print("\r")
+#     count=count+1
+# else:
+#     print("Finished")
+
+num = int(input("rows"))
+for i in range(1, num + 1):
+    print(" " * (num - i) + " * " * i)
