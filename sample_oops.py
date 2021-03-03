9a752c235569ee34b42e35967b9e357cf7c52a82
Adithya Vijay K <adithyavj@gmail.com>
2021-03-03 10:11:54 +0530

    oops concepts

---
+++ sample_oops.py
@@ -0,0 +1,15 @@
+# object oriented Programming
+
+class MySampleClass:
+    def hello(self,n):
+        print("Hello: "+n)
+
+
+# Creating an object of the class
+
+x = MySampleClass()
+name="Adithya Vijay K"
+x.hello(name)
+
+#This can also be done as
+#MySampleClass.hello(x) ,here x is self
